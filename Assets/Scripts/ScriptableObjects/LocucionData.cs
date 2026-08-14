30d194f2b57bcd1a87b615e5f5c3a3852690568e
Paulo Escudero <224657846+pescudero25-droid@users.noreply.github.com>
2026-08-14 10:18:33 -0400

    Scripts de SataliteData, LocucionData y ConfiguraciónVRData

---
+++ Assets/Scripts/ScriptableObjects/LocucionData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LocucionData", menuName = "Scriptable Objects/LocucionData")]
+public class LocucionData : ScriptableObject
+{
+    
+}
---
+++ Assets/Scripts/ScriptableObjects/SateliteData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SateliteData", menuName = "Scriptable Objects/SateliteData")]
+public class SateliteData : ScriptableObject
+{
+    
+}
